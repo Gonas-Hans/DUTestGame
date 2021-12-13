9a66d1fdc315837e8345dc7e14adc616855db361
MAGICFACTORY\agoncharov <gonas.hans@gmail.com>
2021-12-13 17:28:33 +0300

    Upload project

---
+++ Source/DUTestGame/DUTestGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DUTestGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUTESTGAME_API ADUTestGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
